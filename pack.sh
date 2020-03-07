6882f73a7bef8c5ccea0c7badb752b9afb83b3dd
Jeromy Fu <fuji246@gmail.com>
2020-03-06 22:37:32 -0800

    rename to lomo-omxplayer

--- pack.sh
+++ pack.sh
@@ -4,7 +4,7 @@ set -e
 nowDate=$(date +"%Y%m%d")
 commitHash=$(git rev-parse --short HEAD)
 VERSION="$nowDate+$commitHash"
-PACKAGE_NAME="omxplayer"
+PACKAGE_NAME="lomo-omxplayer"
 BUILD_NAME=$PACKAGE_NAME"_"$VERSION
 
 if [ -d $BUILD_NAME ]; then
@@ -21,7 +21,7 @@ Package: $PACKAGE_NAME
 Version: $VERSION
 Section: video
 Priority: optional
-Architecture: all
+Architecture: armhf
 Depends: libasound2 (>= 1.0.16), libavcodec58 (>= 7:4.0), libavformat58 (>= 7:4.1), libavutil56 (>= 7:4.0), libc6 (>= 2.28), libdbus-1-3 (>= 1.9.14), libfreetype6 (>= 2.2.1), libgcc1 (>= 1:3.5), libpcre3, libstdc++6 (>= 5.2), libswresample3 (>= 7:4.0), zlib1g (>= 1:1.1.4), ca-certificates, dbus
 Maintainer: Jeromy Fu<fuji246@gmail.com>
 Description: omxplayer
